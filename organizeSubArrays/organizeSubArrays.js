1df67f1631968fa4b10e4f896f768b6f033b17df
joeyrlee <jleepianist@gmail.com>
2017-04-29 00:44:03 -0700

    (refactor) Refactored O(n) time & space solution

--- organizeSubArrays/organizeSubArrays.js
+++ organizeSubArrays/organizeSubArrays.js
@@ -9,22 +9,19 @@ const organize = arr => arr.sort((a, b) => Number(a[1]) - Number(b[1]));
 
 /* Linear-Time with O(n) space */
 const organize2 = arr => {
-  const alphabet = {
-    'a': 1, 'b': 2, 'c': 3, 'd': 4,
-    'e': 5, 'f': 6, 'g': 7, 'h': 8,
-    'i': 9, 'j': 10, 'k': 11, 'l': 12,
-    'm': 13, 'n': 14, 'o': 15, 'p': 16,
-    'q': 17, 'r': 18, 's': 19, 't': 20,
-    'u': 21, 'v': 22, 'w': 23, 'x': 24,
-    'y': 25, 'z': 26
-  };
-  const results = [],
-        n = arr[arr.length - 1][1],
-        letterCount = alphabet[arr[arr.length - 1][0]] - 1;
-  for (let i = 0; i < n; i++) {
-    results.push(arr[i]);
-    results.push(arr[i + n]);
-    results.push(arr[i + n * letterCount]);
+  const n = Number(arr[arr.length - 1][1]);
+  let aStart = 0;
+  let bStart = n;
+  let cStart = n * 2;
+  const results = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (i % 3 === 0) {
+      results.push(arr[aStart++]);
+    } else if ((i - 1) % 3 === 0) {
+      results.push(arr[bStart++]);
+    } else {
+      results.push(arr[cStart++]);
+    }
   }
   return results;
 };
