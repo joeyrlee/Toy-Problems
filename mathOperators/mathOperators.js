4b64ce6e07c5d71693eb432bf8972f987a810c64
joeyrlee <jleepianist@gmail.com>
2017-05-06 21:36:51 -0700

    (init) Problem and assert unit test fn init

---
+++ mathOperators/mathOperators.js
@@ -0,0 +1,30 @@
+/* Write Multiply, Divide, Modulo, and Power functions */
+
+/* Assert function  */
+const assert = (testNum, expected, actual) => {
+	return expected === actual
+	  ? `Test ${testNum} passed`
+	  : throw `Test ${testNum} Failed
+	  				 expected result: ${expected}
+	  				 actual result: ${}`;
+};
+
+
+const multiply;
+
+/* Multiply Tests */
+
+
+const divide;
+
+/* Divide Tests */
+
+
+const modulo;
+
+/* Modulo Tests */
+
+
+const power;
+
+/* Power Tests */
