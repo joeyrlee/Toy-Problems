92ff73d21fc43b3ccd8fae9ebe796b817d362ec5
joeyrlee <jleepianist@gmail.com>
2017-05-07 22:32:58 -0700

    (feat-update) multiply works for: 0, NaN, and positive inputs

--- mathOperators/mathOperators.js
+++ mathOperators/mathOperators.js
@@ -15,13 +15,36 @@ const assert = (testNum, expected, actual) => {
 const multiply = (a, b) => {
 	if (a === 0 || b === 0) {
 		return 0;
+	} else if (typeof a !== 'number' || typeof b !== 'number') {
+		return NaN;
+	} else if (b > a) {
+		[a, b] = [b, a];
 	}
+
+	let result = a;
+	if (a > 0 && b > 0) {
+		while (b !== 1) {
+			result += a;
+			b--;
+		}
+		return result;
+	}
+	//if negative
 };
 
 /* Multiply Tests */
 assert(1, 0, multiply(0, 0));
 assert(2, 0, multiply(1, 0));
-assert(3, 0, multiply(0, 1));
+
+assert(3, 0, multiply(0, '4'));
+assert(4, 'NaN', multiply(1, '4').toString());
+
+assert(5, 4, multiply(2, 2));
+assert(6, 15, multiply(3, 5));
+
+assert(7, -1, multiply(1, -1));
+assert(8, -5, multiply(1, -5));
+assert(9, 10, multiply(-2, -5));
 
 // const divide;
 
