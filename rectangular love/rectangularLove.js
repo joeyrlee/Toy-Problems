9b776e979c697c2ad1d141ebaa82e1d18f426f1a
joeyrlee <jleepianist@gmail.com>
2017-05-28 13:31:32 -0700

    (feat-update) Pseudocoded high-level solution

---
+++ rectangular love/rectangularLove.js
@@ -0,0 +1,45 @@
+/* Your inputs are configured thusly:
+myRectangle = {
+  btmX = 0,
+  btmY = 0,
+  width = 10,
+  height = 5
+}
+*/
+
+
+/*
+All possible cases (see drawing for a visual): 
+0. no overlap
+1. complete immersion or overlap of rectangles
+2. middle no-corner sectional overlap
+3. corner overlap
+4. middle total pass-through
+*/
+
+//use spaces for github inspection by anthony
+const rectangularLove = (obj1, obj2) => {
+	var newRectangle = {};
+  //swap obj1 and obj2 if necessary so that
+  //obj1.btmX is always less than obj2.btmX
+
+  //no overlap (case 0)
+
+  //complete overlap (case 1)
+
+  //middle no-corner sectional overlap (case 2)
+
+  //corner overlap (case 3)
+    //new btmX is obj1 btmX + width - obj2 btmX - obj1 btmX
+
+    //new btmY is obj1 btmY + height - obj2 btmY
+
+    //newRect width is 
+
+    //newRect height is 
+    
+  //middle total pass-through (case 4)
+
+
+  return newRectangle;
+};
