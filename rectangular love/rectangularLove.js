922a2039a28b63e414aca08f6cf5dab6d3eca339
joeyrlee <jleepianist@gmail.com>
2017-05-28 15:10:08 -0700

    (feat-update) Case 0 tests passing

--- rectangular love/rectangularLove.js
+++ rectangular love/rectangularLove.js
@@ -19,14 +19,20 @@ All possible cases (see drawing for a visual):
 
 //use spaces for github inspection by anthony
 const rectangularLove = (obj1, obj2) => {
-	var newRectangle = {};
+  var newRectangle = {};
   //swap obj1 and obj2 if necessary so that
   //obj1.btmX is always less than obj2.btmX
-
+  if (obj2.btmX < obj1.btmX) {
+    [obj1, obj2] = [obj2, obj1];
+  }
   //no overlap (case 0)
-
+  if (obj1.btmX + obj1.width <= obj2.btmX ||
+  	  obj1.btmY + obj1.height <= obj2.btmY ||
+  	  obj2.btmY + obj2.height <= obj1.btmY) {
+  	return null;
+  }
   //complete overlap (case 1)
-
+  
   //middle no-corner sectional overlap (case 2)
 
   //corner overlap (case 3)
@@ -37,9 +43,40 @@ const rectangularLove = (obj1, obj2) => {
     //newRect width is 
 
     //newRect height is 
-    
+
   //middle total pass-through (case 4)
 
 
   return newRectangle;
 };
+
+/* Unit Tests */
+const assertEquals = (expected, actual, test) => {
+  return expected === actual
+    ? console.log('test passed')
+    : console.log('test failed: \n' + test);
+};
+
+const test1 = [
+  null, 
+  rectangularLove(
+    {
+      btmX: 0,
+      btmY: 0,
+      width: 10,
+      height: 5
+    }, 
+    {
+      btmX: 10,
+      btmY: 0,
+      width: 10,
+      height: 5
+    }
+  ), 
+  "1. it should identify when there's no overlap"
+];
+assertEquals(...test1);
+
+
+
+
