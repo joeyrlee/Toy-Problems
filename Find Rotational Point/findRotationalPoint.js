963679937e35031b13d7a443e439d902913e9d3e
joeyrlee <jleepianist@gmail.com>
2017-06-03 22:26:54 -0700

    (solved) Solved problem in O(logn) time and O(1) space

--- Find Rotational Point/findRotationalPoint.js
+++ Find Rotational Point/findRotationalPoint.js
@@ -4,7 +4,7 @@ words = [
   'supplant',
   'undulate',
   'xenoepist',
-  'asymptote', # <-- rotates here!
+  'asymptote', // <-- rotates here!
   'babka',
   'banoffee',
   'engender',
@@ -13,5 +13,27 @@ words = [
 ];
 
 const findRotationalPoint = words => {
-  
+  let lowerBound = 1;
+  let upperBound = words.length - 1;
+  while (lowerBound <= upperBound) {
+    //3 points of contact: lowerBound, middle, and upperBound
+    //this determines the direction of new guesses in our binary search
+    let guess = Math.floor((lowerBound + upperBound) / 2);
+    if (words[guess].toLowerCase() < words[guess - 1].toLowerCase()) {
+      return guess;
+    } else if (words[guess].toLowerCase() > words[upperBound].toLowerCase()) {
+      lowerBound = guess + 1;
+    } else {
+      upperBound = guess - 1;
+    }
+  }
+  return 0;
 };
+
+const assertEqual = (a, b) => a === b;
+
+assertEqual(words(['a', 'b', 'c', 'd', 'e']), 0);
+assertEqual(words(['e', 'a', 'b', 'c', 'd']), 1);
+assertEqual(words(['d', 'e', 'a', 'b', 'c']), 2);
+assertEqual(words(['c', 'd', 'e', 'a', 'b']), 3);
+assertEqual(words(['b', 'c', 'd', 'e', 'a']), 4);
