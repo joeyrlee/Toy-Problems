8a862685d99a92411696b62c59de2e3a0806d939
joeyrlee <jleepianist@gmail.com>
2017-06-09 17:23:18 -0700

    (feat-update) Solved prompt in O(n) time & space

--- LinkedIn Endorsements/linkedInEndorsements.js
+++ LinkedIn Endorsements/linkedInEndorsements.js
@@ -15,3 +15,46 @@ var expectedResult = [
   { skill: 'html', user: ['Sue'], count: 1 }
 ];
 
+//ex. skills object being constructed
+// {
+//   css: {
+//     user: ['Bill'],
+//     count: 1
+//   }
+// }
+
+//I: array of endorsements by users
+//O: array of endorsements by skills
+//Time complexity of O(n) and space complexity of O(n)
+const organizeEndorsements = endorsements => {
+  const skills = {};
+  //for endorsements
+  for (let i = 0; i < endorsements.length; i++) {
+    //if current endorsements skill exists
+    if (skills.hasOwnProperty(endorsements[i].skill)) {
+      //add endorsements.user to proper skills[endorsements[i]].user array
+      skills[endorsements[i].skill].user.push(endorsements[i].user);
+      //increment the skill's count by 1
+      skills[endorsements[i].skill].count++;
+    //else
+    } else {
+      //initialize endorsement with user: current user and count: 1
+      skills[endorsements[i].skill] = {user: [endorsements[i].user], count: 1};
+    }
+    
+  }
+  
+  const results = [];
+  //for keys in skills
+  for (let skill in skills) {
+    //push into our resultsArray an object with format:
+    results.push(
+      //{ skill: key, user: skills[currKey].user, count: skills[currKey].count }
+      {skill: skill, user: skills[skill].user, count: skills[skill].count}
+    );
+  }
+  
+  return results;
+};
+
+console.log(JSON.stringify(organizeEndorsements(endorsements)) === JSON.stringify(expectedResult));
