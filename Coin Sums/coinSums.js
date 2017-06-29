12e2b8c0e725652f3f9a2307624f01202c39e398
joeyrlee <jleepianist@gmail.com>
2017-06-28 23:55:19 -0700

    (feat-update) Solved problem in O(pn) w/ dp

--- Coin Sums/coinSums.js
+++ Coin Sums/coinSums.js
@@ -1,6 +1,6 @@
 const pieces = [1,2,5,10,20,50,100,200];
 
-/* Brute-force solution */
+/* Brute-force solution with time O(p^n) where p is the number of pieces and n is the amount of money change is being made for */
 const coinSumsBF = target => {
   let total = 0;
   (function recursePieces(currTotal) {
@@ -17,3 +17,47 @@ const coinSumsBF = target => {
   })();
   return total;
 };
+
+/* Time-efficient O(pn) dynamic programming solution */
+//aggregate the coin options for our current total
+const takeWhile = function (arr, predicate) {
+  let result = [];
+  let i = 0;
+  while (predicate(arr[i])) {
+    result.push(arr[i++]);
+  }
+  return result;
+};
+
+//limit the selection of coin choices based on decrementing total
+const possibleChoices = function (total, max) {
+  return takeWhile(coins, function (coin) {
+    return total >= coin && coin <= max;
+  });
+};
+
+const memoize = function (func, context) {
+  const cache = {};
+  return function () {
+    if (!(JSON.stringify(arguments) in cache)) {
+      cache[JSON.stringify(arguments)] = func.apply(context, arguments);
+    }
+    return cache[JSON.stringify(arguments)];
+  };
+};
+                                 
+const makeChange = memoize(function (total, last) {
+  last = last || total;
+  //base case
+  if (total === 0) {
+    return 1;
+  }
+  //recursive case
+  let result = 0;
+  let choices = possibleChoices(total, last);
+  for (let i = 0; i < choices.length; i++) {
+    let coin = choices[i];
+    result += makeChange(total - coin, coin);
+  }
+  return result;
+});
